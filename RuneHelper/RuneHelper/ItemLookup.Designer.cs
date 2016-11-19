af93a2a87f633a23d29da59506d985b52c06c106
Tyler <Tyler@LAPTOP-VGOMAE2N>
2016-11-19 11:06:53 +1000

    Minor Update - added new feauture to the grand exchange item search - added tab page addition - fixed exceptions in code - code maided it

--- RuneHelper/RuneHelper/ItemLookup.Designer.cs
+++ RuneHelper/RuneHelper/ItemLookup.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ItemLookup));
             this.CatagoryBox = new MetroFramework.Controls.MetroComboBox();
             this.SearchButton = new MetroFramework.Controls.MetroButton();
             this.SearchInput = new MetroFramework.Controls.MetroTextBox();
@@ -41,7 +43,25 @@
             this.Description = new MetroFramework.Controls.MetroLabel();
             this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
             this.PriceLabel = new MetroFramework.Controls.MetroLabel();
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.metroTabPage1 = new MetroFramework.Controls.MetroTabPage();
+            this.metroTabPage2 = new MetroFramework.Controls.MetroTabPage();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.PriceTrend = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.CurrentPriceTrend = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel8 = new MetroFramework.Controls.MetroLabel();
+            this.CurrentPriceLabel = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel9 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel6 = new MetroFramework.Controls.MetroLabel();
+            this.ItemIDLabel = new MetroFramework.Controls.MetroLabel();
+            this.StyleManager = new MetroFramework.Components.MetroStyleManager(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.ItemImage)).BeginInit();
+            this.metroTabControl1.SuspendLayout();
+            this.metroTabPage1.SuspendLayout();
+            this.metroTabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.StyleManager)).BeginInit();
             this.SuspendLayout();
             // 
             // CatagoryBox
@@ -143,12 +163,11 @@
             this.Price});
             this.ResultListView.Font = new System.Drawing.Font("Segoe UI", 12F);
             this.ResultListView.FullRowSelect = true;
-            this.ResultListView.Location = new System.Drawing.Point(14, 132);
+            this.ResultListView.Location = new System.Drawing.Point(14, 121);
             this.ResultListView.MultiSelect = false;
             this.ResultListView.Name = "ResultListView";
             this.ResultListView.OwnerDraw = true;
-            this.ResultListView.Size = new System.Drawing.Size(369, 172);
-            this.ResultListView.Style = MetroFramework.MetroColorStyle.Green;
+            this.ResultListView.Size = new System.Drawing.Size(369, 189);
             this.ResultListView.TabIndex = 3;
             this.ResultListView.Theme = MetroFramework.MetroThemeStyle.Light;
             this.ResultListView.UseCompatibleStateImageBehavior = false;
@@ -177,7 +196,7 @@
             // ItemImage
             // 
             this.ItemImage.InitialImage = null;
-            this.ItemImage.Location = new System.Drawing.Point(14, 311);
+            this.ItemImage.Location = new System.Drawing.Point(0, 15);
             this.ItemImage.Name = "ItemImage";
             this.ItemImage.Size = new System.Drawing.Size(93, 89);
             this.ItemImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
@@ -187,16 +206,15 @@
             // ItemNameLabel
             // 
             this.ItemNameLabel.AutoSize = true;
-            this.ItemNameLabel.Location = new System.Drawing.Point(14, 403);
+            this.ItemNameLabel.Location = new System.Drawing.Point(0, 107);
             this.ItemNameLabel.Name = "ItemNameLabel";
-            this.ItemNameLabel.Size = new System.Drawing.Size(58, 19);
+            this.ItemNameLabel.Size = new System.Drawing.Size(0, 0);
             this.ItemNameLabel.TabIndex = 5;
-            this.ItemNameLabel.Text = "Pack Yak";
             // 
             // metroLabel1
             // 
             this.metroLabel1.AutoSize = true;
-            this.metroLabel1.Location = new System.Drawing.Point(113, 311);
+            this.metroLabel1.Location = new System.Drawing.Point(99, 15);
             this.metroLabel1.Name = "metroLabel1";
             this.metroLabel1.Size = new System.Drawing.Size(74, 19);
             this.metroLabel1.TabIndex = 6;
@@ -205,7 +223,7 @@
             // Description
             // 
             this.Description.AutoSize = true;
-            this.Description.Location = new System.Drawing.Point(113, 330);
+            this.Description.Location = new System.Drawing.Point(99, 34);
             this.Description.Name = "Description";
             this.Description.Size = new System.Drawing.Size(0, 0);
             this.Description.TabIndex = 7;
@@ -214,7 +232,7 @@
             // metroLabel2
             // 
             this.metroLabel2.AutoSize = true;
-            this.metroLabel2.Location = new System.Drawing.Point(113, 353);
+            this.metroLabel2.Location = new System.Drawing.Point(-4, 87);
             this.metroLabel2.Name = "metroLabel2";
             this.metroLabel2.Size = new System.Drawing.Size(38, 19);
             this.metroLabel2.TabIndex = 8;
@@ -223,32 +241,184 @@
             // PriceLabel
             // 
             this.PriceLabel.AutoSize = true;
-            this.PriceLabel.Location = new System.Drawing.Point(113, 377);
+            this.PriceLabel.Location = new System.Drawing.Point(0, 106);
             this.PriceLabel.Name = "PriceLabel";
-            this.PriceLabel.Size = new System.Drawing.Size(0, 0);
+            this.PriceLabel.Size = new System.Drawing.Size(16, 19);
             this.PriceLabel.TabIndex = 9;
+            this.PriceLabel.Text = "0";
+            // 
+            // metroTabControl1
+            // 
+            this.metroTabControl1.Controls.Add(this.metroTabPage1);
+            this.metroTabControl1.Controls.Add(this.metroTabPage2);
+            this.metroTabControl1.Location = new System.Drawing.Point(14, 306);
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.SelectedIndex = 0;
+            this.metroTabControl1.Size = new System.Drawing.Size(368, 187);
+            this.metroTabControl1.TabIndex = 10;
+            this.metroTabControl1.UseSelectable = true;
+            // 
+            // metroTabPage1
+            // 
+            this.metroTabPage1.Controls.Add(this.ItemIDLabel);
+            this.metroTabPage1.Controls.Add(this.metroLabel6);
+            this.metroTabPage1.Controls.Add(this.ItemImage);
+            this.metroTabPage1.Controls.Add(this.Description);
+            this.metroTabPage1.Controls.Add(this.ItemNameLabel);
+            this.metroTabPage1.Controls.Add(this.metroLabel1);
+            this.metroTabPage1.HorizontalScrollbarBarColor = true;
+            this.metroTabPage1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroTabPage1.HorizontalScrollbarSize = 10;
+            this.metroTabPage1.Location = new System.Drawing.Point(4, 38);
+            this.metroTabPage1.Name = "metroTabPage1";
+            this.metroTabPage1.Size = new System.Drawing.Size(360, 145);
+            this.metroTabPage1.TabIndex = 0;
+            this.metroTabPage1.Text = "Basic Info";
+            this.metroTabPage1.VerticalScrollbarBarColor = true;
+            this.metroTabPage1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroTabPage1.VerticalScrollbarSize = 10;
+            // 
+            // metroTabPage2
+            // 
+            this.metroTabPage2.Controls.Add(this.CurrentPriceLabel);
+            this.metroTabPage2.Controls.Add(this.metroLabel9);
+            this.metroTabPage2.Controls.Add(this.CurrentPriceTrend);
+            this.metroTabPage2.Controls.Add(this.metroLabel8);
+            this.metroTabPage2.Controls.Add(this.metroLabel5);
+            this.metroTabPage2.Controls.Add(this.metroLabel4);
+            this.metroTabPage2.Controls.Add(this.PriceTrend);
+            this.metroTabPage2.Controls.Add(this.metroLabel3);
+            this.metroTabPage2.Controls.Add(this.PriceLabel);
+            this.metroTabPage2.Controls.Add(this.metroLabel2);
+            this.metroTabPage2.HorizontalScrollbarBarColor = true;
+            this.metroTabPage2.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroTabPage2.HorizontalScrollbarSize = 10;
+            this.metroTabPage2.Location = new System.Drawing.Point(4, 38);
+            this.metroTabPage2.Name = "metroTabPage2";
+            this.metroTabPage2.Size = new System.Drawing.Size(360, 145);
+            this.metroTabPage2.TabIndex = 1;
+            this.metroTabPage2.Text = "Grand Exchange";
+            this.metroTabPage2.VerticalScrollbarBarColor = true;
+            this.metroTabPage2.VerticalScrollbarHighlightOnWheel = false;
+            this.metroTabPage2.VerticalScrollbarSize = 10;
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(-4, 41);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(73, 19);
+            this.metroLabel3.TabIndex = 2;
+            this.metroLabel3.Text = "Price trend";
+            // 
+            // PriceTrend
+            // 
+            this.PriceTrend.AutoSize = true;
+            this.PriceTrend.Location = new System.Drawing.Point(-4, 60);
+            this.PriceTrend.Name = "PriceTrend";
+            this.PriceTrend.Size = new System.Drawing.Size(52, 19);
+            this.PriceTrend.TabIndex = 10;
+            this.PriceTrend.Text = "Neutral";
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.Location = new System.Drawing.Point(-4, 9);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(43, 19);
+            this.metroLabel4.TabIndex = 11;
+            this.metroLabel4.Text = "Today";
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.AutoSize = true;
+            this.metroLabel5.Location = new System.Drawing.Point(136, 9);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(53, 19);
+            this.metroLabel5.TabIndex = 12;
+            this.metroLabel5.Text = "Current";
+            // 
+            // CurrentPriceTrend
+            // 
+            this.CurrentPriceTrend.AutoSize = true;
+            this.CurrentPriceTrend.Location = new System.Drawing.Point(136, 60);
+            this.CurrentPriceTrend.Name = "CurrentPriceTrend";
+            this.CurrentPriceTrend.Size = new System.Drawing.Size(52, 19);
+            this.CurrentPriceTrend.TabIndex = 14;
+            this.CurrentPriceTrend.Text = "Neutral";
+            // 
+            // metroLabel8
+            // 
+            this.metroLabel8.AutoSize = true;
+            this.metroLabel8.Location = new System.Drawing.Point(136, 41);
+            this.metroLabel8.Name = "metroLabel8";
+            this.metroLabel8.Size = new System.Drawing.Size(73, 19);
+            this.metroLabel8.TabIndex = 13;
+            this.metroLabel8.Text = "Price trend";
+            // 
+            // CurrentPriceLabel
+            // 
+            this.CurrentPriceLabel.AutoSize = true;
+            this.CurrentPriceLabel.Location = new System.Drawing.Point(140, 106);
+            this.CurrentPriceLabel.Name = "CurrentPriceLabel";
+            this.CurrentPriceLabel.Size = new System.Drawing.Size(16, 19);
+            this.CurrentPriceLabel.TabIndex = 16;
+            this.CurrentPriceLabel.Text = "0";
+            // 
+            // metroLabel9
+            // 
+            this.metroLabel9.AutoSize = true;
+            this.metroLabel9.Location = new System.Drawing.Point(136, 87);
+            this.metroLabel9.Name = "metroLabel9";
+            this.metroLabel9.Size = new System.Drawing.Size(38, 19);
+            this.metroLabel9.TabIndex = 15;
+            this.metroLabel9.Text = "Price";
+            // 
+            // metroLabel6
+            // 
+            this.metroLabel6.AutoSize = true;
+            this.metroLabel6.Location = new System.Drawing.Point(99, 59);
+            this.metroLabel6.Name = "metroLabel6";
+            this.metroLabel6.Size = new System.Drawing.Size(51, 19);
+            this.metroLabel6.TabIndex = 8;
+            this.metroLabel6.Text = "Item ID";
+            // 
+            // ItemIDLabel
+            // 
+            this.ItemIDLabel.AutoSize = true;
+            this.ItemIDLabel.Location = new System.Drawing.Point(99, 78);
+            this.ItemIDLabel.Name = "ItemIDLabel";
+            this.ItemIDLabel.Size = new System.Drawing.Size(0, 0);
+            this.ItemIDLabel.TabIndex = 9;
+            // 
+            // StyleManager
+            // 
+            this.StyleManager.Owner = this;
             // 
             // ItemLookup
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(405, 500);
-            this.Controls.Add(this.PriceLabel);
-            this.Controls.Add(this.metroLabel2);
-            this.Controls.Add(this.Description);
-            this.Controls.Add(this.metroLabel1);
-            this.Controls.Add(this.ItemNameLabel);
-            this.Controls.Add(this.ItemImage);
+            this.Controls.Add(this.metroTabControl1);
             this.Controls.Add(this.ResultListView);
             this.Controls.Add(this.SearchInput);
             this.Controls.Add(this.SearchButton);
             this.Controls.Add(this.CatagoryBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "ItemLookup";
+            this.Resizable = false;
             this.Text = "ItemLookup";
             this.Load += new System.EventHandler(this.ItemLookup_Load);
             ((System.ComponentModel.ISupportInitialize)(this.ItemImage)).EndInit();
+            this.metroTabControl1.ResumeLayout(false);
+            this.metroTabPage1.ResumeLayout(false);
+            this.metroTabPage1.PerformLayout();
+            this.metroTabPage2.ResumeLayout(false);
+            this.metroTabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.StyleManager)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -267,5 +437,19 @@
         private MetroFramework.Controls.MetroLabel Description;
         private MetroFramework.Controls.MetroLabel metroLabel2;
         private MetroFramework.Controls.MetroLabel PriceLabel;
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private MetroFramework.Controls.MetroTabPage metroTabPage1;
+        private MetroFramework.Controls.MetroTabPage metroTabPage2;
+        private MetroFramework.Controls.MetroLabel PriceTrend;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroLabel CurrentPriceLabel;
+        private MetroFramework.Controls.MetroLabel metroLabel9;
+        private MetroFramework.Controls.MetroLabel CurrentPriceTrend;
+        private MetroFramework.Controls.MetroLabel metroLabel8;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroLabel ItemIDLabel;
+        private MetroFramework.Controls.MetroLabel metroLabel6;
+        private MetroFramework.Components.MetroStyleManager StyleManager;
     }
 }
--- RuneHelper/RuneHelper/ItemLookup.cs
+++ RuneHelper/RuneHelper/ItemLookup.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using MetroFramework.Forms;
-using System.Windows.Forms;
-using System.Net;
+﻿using MetroFramework.Forms;
 using Newtonsoft.Json.Linq;
+using System;
 using System.IO;
+using System.Net;
+using System.Windows.Forms;
 
 namespace RuneHelper
 {
@@ -14,26 +14,52 @@ namespace RuneHelper
             InitializeComponent();
         }
 
-        JObject jObject;
+        private JObject jObject;
+
+        #region Open And Close Functions
 
         private void ItemLookup_Load(object sender, EventArgs e)
         {
+            if (MainForm.SaveData[1] == "light")
+            {
+                StyleManager.Theme = MetroFramework.MetroThemeStyle.Light;
+            }
 
-            
+            if (MainForm.SaveData[1] == "dark")
+            {
+                StyleManager.Theme = MetroFramework.MetroThemeStyle.Dark;
+                this.Theme = StyleManager.Theme;
+            }
+            StyleManager.Style = API.GetColour(MainForm.SaveData[2]);
+            ResultListView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            ResultListView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
         }
+
+        #endregion Open And Close Functions
+
+        #region Form Controls
 
         private void SearchButton_Click(object sender, EventArgs e)
         {
             string Input = SearchInput.Text.Replace(" ", "+");
-            WebClient client = new WebClient();
-            client.Proxy = null;
-            Stream stream = client.OpenRead("http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category="+ CatagoryBox.SelectedIndex + "&alpha="+Input+"&page=1");
-            StreamReader reader = new StreamReader(stream);
-            jObject = JObject.Parse(reader.ReadLine());
-            stream.Close();
-            ResultListView.Items.Clear();
+            try
+            {
+                WebClient client = new WebClient();
+                client.Proxy = null;
+                Stream stream = client.OpenRead("http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=" + CatagoryBox.SelectedIndex + "&alpha=" + Input + "&page=1");
+                StreamReader reader = new StreamReader(stream);
+                jObject = JObject.Parse(reader.ReadLine());
+                stream.Close();
+            }
+            catch
+            {
+
+            }
+            
 
             ResultListView.BeginUpdate();
+            ResultListView.Items.Clear();
+
             int i = 0;
             try
             {
@@ -47,20 +73,24 @@ namespace RuneHelper
             catch
             {
                 ResultListView.EndUpdate();
-            }                        
-            
+            }
         }
 
         private void ResultListView_ItemActivate(Object sender, EventArgs e)
         {
-            
             int number = Convert.ToInt32(ResultListView.SelectedItems[0].Index);
+            // first page of data
             ItemImage.Load((string)jObject["items"][number]["icon_large"]);
             ItemNameLabel.Text = (string)jObject["items"][number]["name"];
             Description.Text = (string)jObject["items"][number]["description"];
+            ItemIDLabel.Text = (string)jObject["items"][number]["id"];
+            // second page
             PriceLabel.Text = (string)jObject["items"][number]["current"]["price"];
-
-
+            PriceTrend.Text = (string)jObject["items"][number]["current"]["trend"];
+            CurrentPriceLabel.Text = (string)jObject["items"][number]["today"]["price"];
+            CurrentPriceTrend.Text = (string)jObject["items"][number]["today"]["trend"];
         }
+
+        #endregion Form Controls
     }
 }
